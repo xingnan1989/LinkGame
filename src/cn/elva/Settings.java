f986b1fdbe0c8b069680013b821f8bb527f4a3d6
xingnan1989 <179350761@qq.com>
2013-12-26 10:06:00 +0800

    连连看2

---
+++ src/cn/elva/Settings.java
@@ -0,0 +1,26 @@
+package cn.elva;
+
+/**
+ * 
+ * @author AXL
+ *这是一个常量结果,用来存储数组的大小
+ */
+public interface Settings
+{
+	//行数
+	public static final int ROWS = 8;
+	//列数
+	public static final int COLUMNS=8;
+	
+	//图片后缀名
+	public static final String RELEX=".gif";
+	
+	//每局所花时间(秒)
+	public static final int PERTIME = 600;
+	
+	//判断的时间间隔
+	public static final int PER = 1;
+	
+	//炸弹的使用次数
+	public static final int BOMBCOUNT = 3;
+}
---
+++ src/cn/elva/model/ArrayPoint.java
@@ -0,0 +1,70 @@
+package cn.elva.model;
+
+public class ArrayPoint
+{
+	//表示行
+	protected int i;
+	//表示列
+	protected int j;
+	//表示值
+	protected int value;
+	
+	public ArrayPoint( int i,int j,int value )
+	{
+		this.i =i;
+		this.j =j;
+		this.value = value;
+	}
+
+	public String toString( )
+	{
+		return i+","+j+","+value;
+	}
+	/**
+	 * @return i
+	 */
+	public int getI()
+	{
+		return i;
+	}
+
+	/**
+	 * @param i 要设置的 i
+	 */
+	public void setI(int i)
+	{
+		this.i = i;
+	}
+
+	/**
+	 * @return j
+	 */
+	public int getJ()
+	{
+		return j;
+	}
+
+	/**
+	 * @param j 要设置的 j
+	 */
+	public void setJ(int j)
+	{
+		this.j = j;
+	}
+
+	/**
+	 * @return value
+	 */
+	public int getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * @param value 要设置的 value
+	 */
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+}
